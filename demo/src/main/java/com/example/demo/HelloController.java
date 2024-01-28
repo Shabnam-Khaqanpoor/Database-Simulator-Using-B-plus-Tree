c6eea10c5acbdab5314cea6f5e5c05ad74af6213
Shabnam2003 <khaghanpourshabnam@gmail.com>
2024-01-28 23:15:16 +0330

    incomplete graphics

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+   @FXML
+    void ClickOnFirst(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("database-view.fxml")));
+        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent);
+        stage.setTitle("Database");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void addDatabase(MouseEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("database-view.fxml")));
+        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent);
+        stage.setTitle("Database");
+        stage.setScene(scene);
+        stage.show();
     }
+
+
 }
