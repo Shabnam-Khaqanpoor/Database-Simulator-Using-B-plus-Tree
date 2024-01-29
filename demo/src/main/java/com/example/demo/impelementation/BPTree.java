e105a5286e943574c3df4cd6518a8481fdcf9d02
Shabnam2003 <khaghanpourshabnam@gmail.com>
2024-01-29 19:06:05 +0330

    new implementation for B+ tree

---
+++ demo/src/main/java/com/example/demo/impelementation/BPTree.java
@@ -0,0 +1,58 @@
+package com.example.demo.impelementation;
+public class BPTree<TKey extends Comparable<TKey>, TValue> {
+    private BTreeNode<TKey> root;
+
+    public BPTree() {
+        this.root = new BTreeLeafNode<TKey, TValue>();
+    }
+
+    /**
+     * Insert a new key and its associated value into the B+ tree.
+     */
+    public void insert(TKey key, TValue value) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+        leaf.insertKey(key, value);
+
+        if (leaf.isOverflow()) {
+            BTreeNode<TKey> n = leaf.dealOverflow();
+            if (n != null)
+                this.root = n;
+        }
+    }
+
+    /**
+     * Search a key value on the tree and return its associated value.
+     */
+    public TValue search(TKey key) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+
+        int index = leaf.search(key);
+        return (index == -1) ? null : leaf.getValue(index);
+    }
+
+    /**
+     * Delete a key and its associated value from the tree.
+     */
+    public void delete(TKey key) {
+        BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+
+        if (leaf.delete(key) && leaf.isUnderflow()) {
+            BTreeNode<TKey> n = leaf.dealUnderflow();
+            if (n != null)
+                this.root = n;
+        }
+    }
+
+    /**
+     * Search the leaf node which should contain the specified key
+     */
+    @SuppressWarnings("unchecked")
+    private BTreeLeafNode<TKey, TValue> findLeafNodeShouldContainKey(TKey key) {
+        BTreeNode<TKey> node = this.root;
+        while (node.getNodeType() == TreeNodeType.InnerNode) {
+            node = ((BTreeInnerNode<TKey>)node).getChild( node.search(key) );
+        }
+
+        return (BTreeLeafNode<TKey, TValue>)node;
+    }
+}
